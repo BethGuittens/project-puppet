7ca0b554ad41eb6fb34be129565d72585f3bf65c
Beth <betholivia4@gmail.com>
2020-03-07 21:13:37 -0500

    changes

--- README.md
+++
@@ -1,3 +0,0 @@
-# project-puppet
-
-This a change to the READ.me file to this puppet project.
--- site/role/manifests/app_server.pp
+++
@@ -1,6 +0,0 @@
-class role:app_server {
-	# This will include the base 
-  include profile::web
-  include profile::base
-  include profile::app
-}
