01df1d2c1b2f77811d72d5c96d3cfe1c5a945324
BethGuittens <bethanyguittensc@gmail.com>
2020-03-08 08:57:39 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC3XK8esFJYY95cxo5dw/szWaiXWQu7Jvhhyia6cvUnJb7H/5QbuYiOJJm6x4hqVzZi51Wxqa07y+9nf8sgbT/05DbHXq5dhyBexuu+iNitIT7lDL4aErz4vZuXGD/7/T3Ejm3l9calo4xAnoxq8xoirfEqLO+SmeHYKMAxSTzrf5wDit3gQoMyLZgGWNJK5i9YVJfa0DjQdgr8apDjHHRh6XzXj3ZEvT3/H0SKGztDj+utdpJdyo2tWXj26NvlHC4ssZuuaFt457kdVBn1K9orPBULg7yo6617t0LfbvC4ezMC5z+MWDZRrIIMHapGg41HBPxpBd8aDan0v+WTQDjt',
   }  
 }
